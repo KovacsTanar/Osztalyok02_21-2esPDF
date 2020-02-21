a5326143f44e72da156c7b251931067fea1acfb8
László Kovács <kovacs.laszlo2@verebelyszki.hu>
2020-02-21 11:23:59 +0100

    Feladat Befejezve

---
+++ Osztalyok02_21/Gomb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok02_21
+{
+    class Gomb
+    {
+        public int r;
+
+        public double Felszin()
+        {
+            double A = (double)4 * r * r * Math.PI;
+            return A;
+        }
+
+        public double Terfogat()
+        {
+            double V = (double)(4 * Math.Pow(r, 3) * Math.PI) / 3;
+            return V;
+        }
+    }
+}
--- Osztalyok02_21/Program.cs
+++ Osztalyok02_21/Program.cs
@@ -10,6 +10,13 @@ namespace Osztalyok02_21
     {
         static void Main(string[] args)
         {
+            Feladat1();
+            Feladat2();
+
+        }
+
+        static void Feladat1() 
+        {
             Dolgozo Pista = new Dolgozo();
             Console.WriteLine("Kérem a Dolgozó Vezetéknevét!");
             Pista.vezeteknev = Console.ReadLine();
@@ -20,10 +27,19 @@ namespace Osztalyok02_21
             Console.WriteLine("Kérem a Dolgozó Születési évét!");
             Pista.szulEv = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("{0} Havi Családi pótléka: {1} Ft.-",Pista.keresztnev,Pista.CsaladiPotlek());
+            Console.WriteLine("{0} Havi Családi pótléka: {1} Ft.-", Pista.keresztnev, Pista.CsaladiPotlek());
             Pista.Nyugdij();
             Console.ReadKey();
-
+            Console.Clear();
+        }
+        static void Feladat2() 
+        {
+            Gomb gomb = new Gomb();
+            Console.WriteLine("Kérem a gömb sugarát!");
+            gomb.r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A gömb felszíne: {0:0.00}",gomb.Felszin());
+            Console.WriteLine("A gömb térfogata: {0:0.00}",gomb.Terfogat());
+            Console.ReadKey();
         }
     }
 }
