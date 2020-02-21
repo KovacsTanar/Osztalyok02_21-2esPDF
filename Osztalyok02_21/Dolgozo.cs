fd66bab4e1159875fa1015d5403e108b63a225ff
László Kovács <kovacs.laszlo2@verebelyszki.hu>
2020-02-21 11:00:42 +0100

    Initial Commit

---
+++ Osztalyok02_21/Dolgozo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok02_21
+{
+    class Dolgozo
+    {
+        public string vezeteknev;
+        public string keresztnev;
+        public int gyermekekSzama;
+        public int szulEv;
+
+        public int CsaladiPotlek() 
+        {
+            int fizetendo;
+            if (gyermekekSzama == 1)
+                fizetendo = 12200;
+            else if (gyermekekSzama == 2)
+                fizetendo = 13400 * gyermekekSzama;
+            else if (gyermekekSzama >= 3)
+                fizetendo = 16000 * gyermekekSzama;
+            else
+                fizetendo = 0;
+
+            return fizetendo;
+        }
+
+        public void Nyugdij() 
+        {
+            int ev = 2020 - szulEv;
+            if(ev < 63)
+                Console.WriteLine("Még {0} év van a nyugdíjig!",63- ev);
+            else
+                Console.WriteLine("Már Nyugdíjas vagy!");
+        }
+    }
+}
--- Osztalyok02_21/Program.cs
+++ Osztalyok02_21/Program.cs
@@ -10,6 +10,19 @@ namespace Osztalyok02_21
     {
         static void Main(string[] args)
         {
+            Dolgozo Pista = new Dolgozo();
+            Console.WriteLine("Kérem a Dolgozó Vezetéknevét!");
+            Pista.vezeteknev = Console.ReadLine();
+            Console.WriteLine("Kérem a Dolgozó Keresztnevét!");
+            Pista.keresztnev = Console.ReadLine();
+            Console.WriteLine("Kérem a Dolgozó gyerekeinek a számát!");
+            Pista.gyermekekSzama = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem a Dolgozó Születési évét!");
+            Pista.szulEv = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} Havi Családi pótléka: {1} Ft.-",Pista.keresztnev,Pista.CsaladiPotlek());
+            Pista.Nyugdij();
+            Console.ReadKey();
 
         }
     }
